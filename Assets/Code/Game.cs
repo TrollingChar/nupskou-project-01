64d5e3c82502640f34cd0bffca577b0f9da5bc5c
trollingchar <dmitriyrodin0@gmail.com>
2019-08-27 11:32:45 +0300

    заготовка для экрана игры

---
+++ Assets/Code/Game.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEngine;
+
+
+public class Game : MonoBehaviour {
+
+
+    [SerializeField] private TMP_Text timeText;
+    
+
+    private int t = 0;
+
+
+    public int Time {
+        get { return t; }
+        private set {
+            if (t == value) return;
+            t = value;
+            if (timeText) timeText.text = $"{value / 3600}:{value / 60 % 60:00}:{value % 60:00}"; // m:ss:tt
+        }
+    }
+
+
+    void Awake () {
+        _.Game = this;
+    }
+
+
+    void Update () {
+        Time++;
+    }
+
+}
---
+++ Assets/Code/_.cs
@@ -0,0 +1,6 @@
+public static class _ {
+
+    public static Difficulty Difficulty = Difficulty.Normal;
+    public static Game       Game;
+
+}
