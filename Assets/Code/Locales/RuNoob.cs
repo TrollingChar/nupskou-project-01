49fe6e7d53eadaf694c9ef043d0e0838d5b598f0
trollingchar <dmitriyrodin0@gmail.com>
2019-09-04 17:00:15 +0300

    добавил нубский язык

---
+++ Assets/Code/Locales/RuNoob.cs
@@ -0,0 +1,17 @@
+namespace Code {
+
+    public class RuNoob : Locale {
+
+        public override string Easy     => "Лигка";
+        public override string Normal   => "Нормас";
+        public override string Hard     => "Сложна";
+        public override string Trolling => "Траллинг";
+        
+        public override string Lives    => "Жызне";
+        public override string Bombs    => "Читы";
+        public override string Score    => "Лойсы";
+        public override string Time     => "Вримя";
+
+    }
+
+}
